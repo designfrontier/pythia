2e035db649b4f66a8a33b8af564650727a55c02f
pjozsef <9980358+pjozsef@users.noreply.github.com>
2019-07-08 12:18:13 +0200

    test(config): add test cases for parsing config file

---
+++ modules/read-config.test.js
@@ -0,0 +1,45 @@
+const test = require('ava'),
+readConfig = require('./read-config');
+
+test('for an empty config file', t => {
+  t.deepEqual(readConfig('fixtures/.empty-config'), {
+    threshold: 20,
+    exclude: {
+      users: [],
+      directories: [],
+      files: []
+    }
+  });
+});
+
+test('for a malformed config file', t => {
+  t.throws(() => readConfig('fixtures/.malformed-config'), SyntaxError);
+});
+
+test('for a config file with threshold', t => {
+  t.is(readConfig('fixtures/.threshold-config').threshold, 42);
+});
+
+test('for a config file with excluded users', t => {
+  t.deepEqual(readConfig('fixtures/.exclude-users-config').exclude.users, ['test1@example.com', 'test2@example.com']);
+});
+
+test('for a config file with excluded directories', t => {
+  t.deepEqual(readConfig('fixtures/.exclude-directories-config').exclude.directories, ['dir-one', 'dir-two']);
+});
+
+test('for a config file with excluded files', t => {
+  t.deepEqual(readConfig('fixtures/.exclude-files-config').exclude.files, ['file-one', 'file-two']);
+});
+
+test('for a full featured config file', t => {
+  t.deepEqual(readConfig('fixtures/.pythia-config'), {
+    threshold: 43,
+    exclude: {
+      users: ['test1@example.com', 'test2@example.com'],
+      directories: ['dir-one', 'dir-two'],
+      files: ['file-one', 'file-two']
+    }
+  });
+});
+
