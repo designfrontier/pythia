f1f9a636ff0dda3d4d031ee1a16e162c2f973a7d
Daniel Sellers <daniel@designfrontier.net>
2019-10-29 20:49:10 -0600

    cleans up lints
    well... disables jest lints in ava files

--- modules/file-is-excluded.test.js
+++ modules/file-is-excluded.test.js
@@ -1,3 +1,5 @@
+/* eslint-disable jest/expect-expect, jest/no-test-callback */
+
 const test = require('ava');
 const fileIsExcluded = require('./file-is-excluded');
 
--- modules/read-config.test.js
+++ modules/read-config.test.js
@@ -1,3 +1,5 @@
+/* eslint-disable jest/expect-expect, jest/no-test-callback */
+
 const test = require('ava');
 const readConfig = require('./read-config');
 
